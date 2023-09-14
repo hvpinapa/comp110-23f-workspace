78d33eef2b7284a3084167bfa959afe7b88cdca0
Hari Pinapaka <hvpinapa@email.unc.edu>
2023-09-14 18:18:23 -0400

    changed ex02 to be finished

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -7,42 +7,42 @@ i: int = len(secret_word)
 guess: str = input(f"What is your {i} letter guess? ")
 
 while len(guess) != i:
-    guess: str = input(f"That was not {i} letters! Try again: ")
+    guess = input(f"That was not {i} letters! Try again: ")
 
-#Initializing Color Boxes
+# Initializing Color Boxes
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-#Establishing variables for index check
+# Establishing variables for index check
 idx: int = 0
 box_check: str = ""
 
-#Logic Behind Index Check for Positions in Secret Word
+# Logic Behind Index Check for Positions in Secret Word
 while idx < i:
-    #Check if index is the same -- green box
+    # Check if index is the same -- green box
     if guess[idx] == secret_word[idx]:
         box_check += GREEN_BOX
 
-    #Check if index is in string but diff position -- yellow box
+    # Check if index is in string but diff position -- yellow box
     else:
         alt_idx: int = 0
-        guess_chr: bool = False
-        while not guess_chr and (alt_idx < i):
+        tried_chr: bool = False
+        while not tried_chr and (alt_idx < i):
             if guess[idx] == secret_word[alt_idx]:
-                guess_chr = True
+                tried_chr = True
                 box_check += YELLOW_BOX
             alt_idx += 1
 
-    #Check if index is not in the string -- white box
-        if not guess_chr:
+    # Check if index is not in the string -- white box
+        if not tried_chr:
             box_check += WHITE_BOX
 
     idx += 1     
-print (box_check) 
+print(box_check) 
 
-#Output Logic Behind Guess
+# Output Logic Behind Guess
 if guess == secret_word:
     print("Woo! You got it!")        
 else:
-    print ("Not quite. Play again soon!")
+    print("Not quite. Play again soon!")
